1cf88a74944f2afd641fbfe338564909be063ae4
gabo8090 <g-araujo94@hotmail.com>
2019-05-29 11:56:22 -0300

    Gabriel Oliveira

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,9 +2,11 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.londonappbrewery.destini.models.Answer;
+import com.londonappbrewery.destini.models.Story;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     Button mAnswerTop;
     Button mAnswerBottom;
     // TODO: Declare as variaveis aqui:
+
+    Story mT1 = new Story(R.string.T1_Story);
+    Story mT2 = new Story(R.string.T2_Story);
+    Story mT3 = new Story(R.string.T3_Story);
+    Story mT4 = new Story(R.string.T4_End);
+    Story mT5 = new Story(R.string.T5_End);
+    Story mT6 = new Story(R.string.T6_End);
+
+    Answer mT1_Ans1 = new Answer(R.string.T1_Ans1);
+    Answer mT1_Ans2 = new Answer(R.string.T1_Ans2);
+    Answer mT2_Ans1 = new Answer(R.string.T2_Ans1);
+    Answer mT2_Ans2 = new Answer(R.string.T2_Ans2);
+    Answer mT3_Ans1 = new Answer(R.string.T3_Ans1);
+    Answer mT3_Ans2 = new Answer(R.string.T3_Ans2);
+
+
 
     //indice corrente da historia
     private Story mStorySelected;
@@ -24,8 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         //TODO: Faça o link do layout com a activity
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mAnswerTop = findViewById(R.id.buttonTop);
+        mAnswerBottom = findViewById(R.id.buttonBottom);
+
 
         //TODO:faça o mapeamento da história
+        mT1.setAnswerTop(mT1_Ans1);
+
 
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
